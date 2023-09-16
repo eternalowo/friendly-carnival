3bb1ae8f10b17b0d7a6e77ad11a29a78ea5dcc03
eternalowo <jeppa2@yandex.ru>
2023-09-16 16:40:45 +0700

    removed 'get' method due to encapsulation issues, made 'size' method public

--- abstract_list.h
+++ abstract_list.h
@@ -12,11 +12,10 @@ namespace linked_list {
 		virtual void insert(T value, T ins) = 0;
 		virtual void remove(T value) = 0;
 		virtual T pop(T value) = 0;
-		virtual ListNode<T>* get(T value) const = 0;
-		virtual void update(T value_old, T value_new) const = 0;
+		virtual void update(T value_old, T value_new) = 0;
+		virtual std::size_t size() const = 0;
 
 	private:
-		virtual std::size_t size() const = 0;
 		virtual bool is_empty() const = 0;
 	};
 
